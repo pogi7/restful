33281f8af93ff786e0d112c06dc6889352daca2e
pogi7 <aaronlevitt7@gmail.com>
2022-09-25 21:21:14 -0700

    working version of payload POST github API

--- main.js
+++ main.js
@@ -2,20 +2,23 @@
 // https://github.com/octokit/core.js#readme
 
 const GH_TOKEN = process.env.GH_TOKEN;
+const fs = require("fs");
+
+const payload = fs.readFileSync("payload.md", 'utf-8');
 
 const { Octokit } = require("@octokit/rest")
 const octokit = new Octokit({
     auth: GH_TOKEN
   })
   
-  await octokit.request('POST /repos/pogi7/restful/releases', {
-    owner: 'pogi7',
-    repo: 'restful',
-    tag_name: '1.0.0',
-    target_commitish: 'main',
-    name: '1.0.0',
-    body: 'Description of the release',
-    draft: false,
-    prerelease: false,
-    generate_release_notes: true
-  })
+octokit.request('POST /repos/pogi7/restful/releases', {
+  owner: 'pogi7',
+  repo: 'restful',
+  tag_name: '1.0.0',
+  target_commitish: 'main',
+  name: '1.0.0',
+  body: payload.toString(),
+  draft: false,
+  prerelease: false,
+  generate_release_notes: true
+})
--- payload.md
+++ payload.md
@@ -1,1 +1,2 @@
 This is a test
+Including this one
